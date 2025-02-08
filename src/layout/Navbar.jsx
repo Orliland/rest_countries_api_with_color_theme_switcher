3e0c338ad5f919fb29a8eb8a053d4e11e54cc9cf
Orlando Verdin <git@orli.land>
2025-02-08 14:41:25 -0700

    fix font-weight

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -3,7 +3,7 @@ import ThemeSwitcher from "../components/ThemeSwitcher";
 const Navbar = () => {
   return (
     <nav className="bg-white flex justify-between items-center py-[30px] px-4 shadow-light">
-      <h1 className="text-light-text text-sm font-extrabold leading-[20px]">
+      <h1 className="text-light-text text-sm font-[800] leading-[20px]">
         Where in the world?
       </h1>
       <ThemeSwitcher />
