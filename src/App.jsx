f46b08ffce83a236c4ef4b726aa4a76c29afa721
Orlando Verdin <git@orli.land>
2025-02-08 19:49:03 -0700

    add wrapper for very large screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import "./App.css";
-
 import Navbar from "./layout/Navbar";
 
 function App() {
   return (
-    <div className="text-light-text ">
+    <div className="text-light-text">
       <header>
         <Navbar />
       </header>
-      <h1>REST Countries Api with Color Theme Switcher</h1>
+      <div className="mx-auto max-w-[1440px] px-4 md:px-20">
+        <h1>REST Countries Api with Color Theme Switcher</h1>
+      </div>
     </div>
   );
 }
