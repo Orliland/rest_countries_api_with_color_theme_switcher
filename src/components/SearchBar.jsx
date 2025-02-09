987d36d7cdfaa29544aa3f21b4980e4e61872bd9
Orlando Verdin <git@orli.land>
2025-02-09 07:49:58 -0700

    add dark styles to searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,10 +12,10 @@ const SearchBar = () => {
       <input
         type="text"
         placeholder="Search for a country..."
-        className="shadow-input outline-light-input text-light-input w-full rounded-[5px] border-none bg-white px-8 py-3.5 pl-[74px] text-xs leading-5"
+        className="shadow-input outline-light-input text-light-input placeholder:text-light-input dark:bg-dark-elements w-full rounded-[5px] border-none bg-white px-8 py-3.5 pl-[74px] text-xs leading-5 focus:outline-[1px] dark:text-white dark:outline-white dark:placeholder:text-white"
       />
       <div className="absolute top-0 left-8 flex h-full items-center justify-center">
-        <FaSearch className="h-4 w-4 text-[#B2B2B2]" />
+        <FaSearch className="text-light-input h-4 w-4 dark:text-white" />
       </div>
     </form>
   );
