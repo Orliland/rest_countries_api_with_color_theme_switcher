fb97f8115447a406be37220e9f1fd3d874cb42c3
Orlando Verdin <git@orli.land>
2025-02-09 07:28:50 -0700

    add wrapper to navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -2,9 +2,9 @@ import ThemeSwitcher from "../components/ThemeSwitcher";
 
 const Navbar = () => {
   return (
-    <nav className="shadow-light">
-      <div className="mx-auto flex max-w-[1440px] items-center justify-between bg-white px-4 py-[30px] md:px-20 md:py-6">
-        <h1 className="text-light-text md:leading-auto text-sm leading-[20px] font-[700] md:text-2xl">
+    <nav className="shadow-light dark:bg-dark-elements text-light-text bg-white dark:text-white">
+      <div className="mx-auto flex max-w-[1440px] items-center justify-between px-4 py-[30px] md:px-20 md:py-6">
+        <h1 className="md:leading-auto text-sm leading-[20px] font-[700] md:text-2xl">
           Where in the world?
         </h1>
         <ThemeSwitcher />
