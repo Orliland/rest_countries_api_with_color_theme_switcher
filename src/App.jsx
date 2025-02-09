f254e4be9425430573aa7487050864296d46240a
Orlando Verdin <git@orli.land>
2025-02-08 19:30:07 -0700

    add localstorage for dark mode track

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import Navbar from "./layout/Navbar";
 
 function App() {
   return (
-    <>
+    <div className="text-light-text ">
       <header>
         <Navbar />
       </header>
       <h1>REST Countries Api with Color Theme Switcher</h1>
-    </>
+    </div>
   );
 }
 
--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -1,19 +1,34 @@
 import { IoMoonOutline, IoMoon } from "react-icons/io5";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-// TODO: add desktop styles, dark styles and local storage for theme
+// TODO: add desktop styles, dark styles
 
 const ThemeSwitcher = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+
+    if (theme) {
+      setIsDarkMode(theme === "dark");
+    }
+  }, [isDarkMode]);
+
   const toggleTheme = () => {
     setIsDarkMode(!isDarkMode);
+    localStorage.setItem("theme", isDarkMode ? "light" : "dark");
   };
 
   return (
     <button className="flex items-center gap-2" onClick={toggleTheme}>
       {isDarkMode ? <IoMoon /> : <IoMoonOutline />}
-      <span className="text-xs font-[500]">Dark Mode</span>
+      <span className="text-xs font-[500] ">Dark Mode</span>
     </button>
   );
 };
