46aec216a238d8fe89e6f08ced60173b74a98f8f
Orlando Verdin <git@orli.land>
2025-02-09 22:38:31 -0700

    create a component for FilterOptions

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,5 +1,13 @@
 import { MdKeyboardArrowDown } from "react-icons/md";
 
+const FilterOption = ({ name }) => {
+  return (
+    <li className="leading-0">
+      <button className="text-xs dark:text-white">{name}</button>
+    </li>
+  );
+};
+
 const Filter = () => {
   return (
     <div className="shadow-input dark:bg-dark-elements relative w-[200px] rounded-[5px] bg-white py-3.5 pr-[18px] pl-6">
@@ -8,21 +16,11 @@ const Filter = () => {
         <MdKeyboardArrowDown className="text-dark-elements h-2.5 w-2.5 dark:text-white" />
       </div>
       <ul className="dark:bg-dark-elements shadow-input absolute top-[calc(100%+4px)] left-0 flex w-full flex-col gap-2 rounded-[5px] bg-white px-6 py-4">
-        <li className="leading-0">
-          <button className="text-xs dark:text-white">Africa</button>
-        </li>
-        <li className="leading-0">
-          <button className="text-xs leading-4 dark:text-white">America</button>
-        </li>
-        <li className="leading-0">
-          <button className="text-xs leading-4 dark:text-white">Asia</button>
-        </li>
-        <li className="leading-0">
-          <button className="text-xs leading-4 dark:text-white">Europe</button>
-        </li>
-        <li className="leading-0">
-          <button className="text-xs leading-4 dark:text-white">Oceania</button>
-        </li>
+        <FilterOption name="Africa" />
+        <FilterOption name="America" />
+        <FilterOption name="Asia" />
+        <FilterOption name="Europe" />
+        <FilterOption name="Oceania" />
       </ul>
     </div>
   );
