54df0d6cb8a724c2d4a87d90d1846ea2d13272b1
Orlando Verdin <git@orli.land>
2025-02-13 21:52:51 -0700

    fix line height of logo

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="shadow-light dark:bg-dark-elements text-light-text bg-white dark:text-white">
       <div className="mx-auto flex max-w-[1440px] items-center justify-between px-4 py-[30px] md:px-20 md:py-6">
-        <h1 className="md:leading-auto text-sm leading-[20px] font-[700] md:text-2xl">
+        <h1 className="text-sm leading-[20px] font-[700] md:text-2xl md:leading-[33px]">
           <Link to="/">Where in the world?</Link>
         </h1>
         <ThemeSwitcher />
